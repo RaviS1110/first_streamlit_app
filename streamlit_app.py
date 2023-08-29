9b45cefe78980f223222e5e2bed9199c4a1e9fbf
RaviS1110 <38092288+RaviS1110@users.noreply.github.com>
2023-08-29 19:20:10 +0530

    Update streamlit_app.py
    BreakFast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parent new Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('kale,Spinach & Rocket smoothie')
+streamlit.text('Hard Boiled free-Range Egg')
